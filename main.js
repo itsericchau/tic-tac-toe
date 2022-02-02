7365d0f3f5999b20a9c62e401a9a969ab9dd6a04
cchau <chun.wing.chau@gmail.com>
2022-02-02 11:08:37 +1100

    editing plan.txt

--- main.js
+++
@@ -1,9 +0,0 @@
-// declare variables for two players p1 p2
-// declare winning states with function call
-
-// within function call have winning states checked
-// let player place an X when first div box is clicked then O and rotate
-// everytime div box is clicked check if state is win/draw 
-// if three in a row its a win otherwise if grid is full or 9 clicks then game ends
-// 
-// 
